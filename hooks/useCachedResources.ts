1829c0a4d8e9ec16867a4eedd4b677fe4fe7705a
Omar Shatani <omar.shatani96@gmail.com>
2022-03-08 17:25:50 +0100

    updated fonts loading

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,33 +1,60 @@
-import { FontAwesome } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { FontAwesome } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Montserrat_100Thin,
+  Montserrat_200ExtraLight,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import {
+  Roboto_100Thin,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from "@expo-google-fonts/roboto";
+import { useEffect, useState } from "react";
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
+  let [fontsLoaded] = useFonts({
+    ...FontAwesome.font,
+    "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Montserrat_100Thin,
+    Montserrat_300Light,
+    Montserrat_200ExtraLight,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
+  SplashScreen.preventAutoHideAsync();
+
   // Load any resources or data that we need prior to rendering the app
   useEffect(() => {
     async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHideAsync();
-
-        // Load fonts
-        await Font.loadAsync({
-          ...FontAwesome.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
+      if (fontsLoaded) {
         SplashScreen.hideAsync();
+        setLoadingComplete(true);
       }
     }
 
     loadResourcesAndDataAsync();
-  }, []);
+  }, [fontsLoaded]);
 
   return isLoadingComplete;
 }
