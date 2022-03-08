a2f479a90a81f97168061c79c8db8c19f2c5b6dd
Omar Shatani <omar.shatani96@gmail.com>
2022-03-08 14:43:42 +0100

    created HomeScreen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+export default function HomeScreen(props: RootTabScreenProps<"Home">) {
+  return <View></View>;
+}
