c74c76e524b5cac5a9aa75ba6b89754d90a9153d
Omar Shatani <omar.shatani96@gmail.com>
2022-03-16 19:15:21 +0100

    added hotel type

--- types.tsx
+++ types.tsx
@@ -36,3 +36,31 @@ export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
     BottomTabScreenProps<RootTabParamList, Screen>,
     NativeStackScreenProps<RootStackParamList>
   >;
+
+export type Hotel = {
+  id: number;
+  name: string;
+  location: {
+    address: string;
+    city: string;
+    latitude: number;
+    longitude: number;
+  };
+  stars: number;
+  checkIn: {
+    from: string;
+    to: string;
+  };
+  checkOut: {
+    from: string;
+    to: string;
+  };
+  contact: {
+    phoneNumber: string;
+    email: string;
+  };
+  gallery: Array<string>;
+  userRating: number;
+  price: number;
+  currency: string;
+};
