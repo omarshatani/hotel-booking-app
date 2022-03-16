bfe6d7e722bc27d371a62bab46b6826ea68b5a1f
Omar Shatani <omar.shatani96@gmail.com>
2022-03-16 19:17:32 +0100

    added best offers section

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { ScaledSheet } from "react-native-size-matters";
 import { Text, View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
+import { Hotel, RootTabScreenProps } from "../types";
 import { useNavigation } from "@react-navigation/native";
 import Section from "../components/Section";
 import Searchbar from "../components/Seachbar";
 import { getHotelsDetails } from "../api/hotels";
 import HorizontalCardsList from "../components/HorizontalCardsList";
+import { FlatList, ScrollView } from "react-native";
+import LongCard from "../components/LongCard";
+import Divider from "../components/Divider";
 
 export default function HomeScreen(props: RootTabScreenProps<"Home">) {
   const navigation = useNavigation();
-  const [hotels, setHotels] = React.useState<any>([]);
+  const [hotels, setHotels] = React.useState<Hotel[]>([]);
   const [loading, setLoading] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     (async () => {
       setLoading(true);
       try {
-        const response = (await getHotelsDetails()).data;
+        const response: Hotel[] = (await getHotelsDetails()).data;
         setHotels(response);
         setLoading(false);
       } catch (err) {
@@ -30,12 +33,33 @@ export default function HomeScreen(props: RootTabScreenProps<"Home">) {
   if (loading) return <View />; //TODO: migliorare gestione
 
   return (
-    <View style={styles.container}>
-      <Searchbar containerStyle={styles.searchBar} />
-      <Section title="Popular">
+    <ScrollView
+      style={styles.container}
+      nestedScrollEnabled={true}
+      showsVerticalScrollIndicator={false}
+    >
+      <Section
+        title="Popular Hotels"
+        containerStyle={{ paddingHorizontal: 0 }}
+        titleContainerStyle={styles.titleContainerStyle}
+      >
         <HorizontalCardsList data={hotels} />
       </Section>
-    </View>
+      <Section
+        title="Best offers"
+        containerStyle={{ paddingHorizontal: 0 }}
+        titleContainerStyle={styles.titleContainerStyle}
+      >
+        <FlatList
+          data={hotels.sort((a, b) => a.price - b.price)}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => <Divider horizontal />}
+          renderItem={({ item }) => <LongCard {...item} />}
+          contentContainerStyle={styles.flatListStyle}
+          scrollEnabled={false}
+        />
+      </Section>
+    </ScrollView>
   );
 }
 
@@ -45,6 +69,12 @@ const styles = ScaledSheet.create({
   },
   searchBar: {
     marginVertical: "12@vs",
-    paddingHorizontal: "10@s",
+  },
+  titleContainerStyle: {
+    paddingHorizontal: "20@s",
+  },
+  flatListStyle: {
+    paddingVertical: "5@s",
+    paddingHorizontal: "25@s",
   },
 });
