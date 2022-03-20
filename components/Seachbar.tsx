d79c49eee6c8d1fec688b8dea9f4a34bfa764c48
Omar Shatani <omar.shatani96@gmail.com>
2022-03-20 20:13:14 +0100

    impemented search

--- components/Seachbar.tsx
+++ components/Seachbar.tsx
@@ -1,40 +1,50 @@
 import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import React from "react";
 import { Platform, Pressable, RegisteredStyle } from "react-native";
 import { ScaledSheet } from "react-native-size-matters";
 import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
+import { RootTabScreenProps } from "../types";
 import { Text, TextInput, View, ViewProps } from "./Themed";
 
 export default function Searchbar({
   filterVisible = true,
   containerStyle,
+  navigation,
 }: {
   filterVisible?: boolean;
   containerStyle?: RegisteredStyle<ViewProps & Record<never, number>>;
-}) {
+} & NativeStackHeaderProps) {
   const colorScheme = useColorScheme();
+  const handleSeach = (text: string) => {
+    navigation.setParams({
+      query: text,
+    });
+  };
   return (
     <View style={[styles.container, containerStyle]}>
       <View style={[styles.box, styles.shadow]}>
-        <TextInput placeholder="Search" medium />
+        <TextInput placeholder="Search" medium onChangeText={handleSeach} />
       </View>
-      <Pressable
-        android_ripple={{
-          borderless: false,
-        }}
-        style={[
-          styles.filterButton,
-          styles.shadow,
-          { backgroundColor: Colors.light.primary },
-        ]}
-      >
-        <FontAwesome5
-          name="filter"
-          size={15}
-          color={Colors[colorScheme].background}
-        />
-      </Pressable>
+      {filterVisible && (
+        <Pressable
+          android_ripple={{
+            borderless: false,
+          }}
+          style={[
+            styles.filterButton,
+            styles.shadow,
+            { backgroundColor: Colors.light.primary },
+          ]}
+        >
+          <FontAwesome5
+            name="filter"
+            size={15}
+            color={Colors[colorScheme].background}
+          />
+        </Pressable>
+      )}
     </View>
   );
 }
