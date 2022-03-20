1eee9823f69230bd05362de126f3b7c611b7cc47
Omar Shatani <omar.shatani96@gmail.com>
2022-03-20 22:20:15 +0100

    minor ui fix

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -4,12 +4,20 @@ import { View } from "./Themed";
 const Divider = ({
   horizontal,
   vertical,
+  color,
 }: {
   horizontal?: boolean;
   vertical?: boolean;
+  color?: string;
 }) => {
   return (
-    <View style={[vertical && { width: 20 }, horizontal && { height: 20 }]} />
+    <View
+      style={[
+        vertical && { width: 1, marginHorizontal: 10 },
+        horizontal && { height: 1, marginVertical: 10 },
+        { backgroundColor: color ?? "transparent" },
+      ]}
+    />
   );
 };
 
